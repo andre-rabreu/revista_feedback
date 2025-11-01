abd3701bf9869ced432761d5234ae80648999b71
andre-rabreu <andre-rabreu@users.noreply.github.com>
2025-11-01 00:10:30 +0000

    deploy: a2542652b921214664bb639c97ff9cc68e054b29

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "253728145"
+    serviceWorkerVersion: "2099151286"
   }
 });
